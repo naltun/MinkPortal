703804b1d8967e561aa3ffbc6ad08471af66f874
naltun <nbaltunian@gmail.com>
2015-06-25 17:13:41 +0100

    Update ability
    ability.rb does not work.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   #require 'DatePickerInput'
+
+	rescue_from CanCan::AccessDenied do |exception|
+	  flash[:error] = "Access denied."
+	  redirect_to root_url
+	end
 end
--- app/controllers/tsightings_controller.rb
+++ app/controllers/tsightings_controller.rb
@@ -1,5 +1,6 @@
 class TsightingsController < ApplicationController
   before_action :set_tsighting, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   # GET /tsightings
   # GET /tsightings.json
