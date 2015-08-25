5511996f9d05c98539646a2e60e1482a9683d858
worldinmyeyes <wojciechszczerek@wp.pl>
2015-08-25 16:38:11 +0100

    started another batch of corrections

--- app/models/raft.rb
+++ app/models/raft.rb
@@ -2,10 +2,13 @@ class Raft < ActiveRecord::Base
 	belongs_to :user
 	validates :x_cord, presence: true
 	validates :y_cord, presence: true
+    validates_length_of :y_cord, :minimum => 6, :maximum => 6, :numericality => true
+    validates_length_of :y_cord, :minimum => 6, :maximum => 6, :numericality => true
     validates :river, presence: true
     validates :catchment, presence: true
     validates :start_date, presence: true
     validates :raft_code, presence: true
+    validates_uniqueness_of :raft_code
     validates :raft_or_tunnel, presence: true
     validates :status_change_date, presence: true
     validates :raft_status, presence: true
